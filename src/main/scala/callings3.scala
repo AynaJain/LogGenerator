2438a0b7eec355de9ee44b0018aff3a569fb747b
Ayna Jain <aynajain03@gmail.com>
2021-11-06 19:54:33 -0500

    change on logback

--- src/main/scala/callings3.scala
+++ src/main/scala/callings3.scala
@@ -5,17 +5,21 @@ import com.typesafe.config.ConfigFactory
 
 import java.io.File
 
-object callings3:
+//Reference: https://codedestine.com/aws-s3-putobject-java/
+//Reference: https://stackoverflow.com/questions/42230086/writing-a-file-into-amazon-s3-aws-storage-using-scala
+
+object callings3 {
   val config = ConfigFactory.load("application")
-  val bucket_name: String = config.getString("s3utilities.s3bucket")
-  val file_path: String = config.getString("s3utilities.file_path")
-  val key_name: String = config.getString("s3utilities.key")
+  val bucketName: String = config.getString("s3utilities.s3bucket")
+  val filePath: String = config.getString("s3utilities.file_path")
+  val fileName: String = config.getString("s3utilities.key")
 
   val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_1).build
-  try s3.putObject(bucket_name, key_name, new File(file_path))
+  try s3.putObject(bucketName, filePath, new File(fileName))
 
   catch {
     case e: AmazonServiceException =>
       System.err.println(e)
       System.exit(1)
   }
+}
