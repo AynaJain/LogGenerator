8b438286b543902de180b12b7a7db561d83a3f88
Ayna Jain <aynajain03@gmail.com>
2021-11-06 20:25:39 -0500

    small logback change

--- src/main/scala/callings3.scala
+++ src/main/scala/callings3.scala
@@ -1,25 +1,24 @@
-import com.amazonaws.AmazonServiceException
-import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
-import com.amazonaws.regions.Regions
-import com.typesafe.config.ConfigFactory
-
-import java.io.File
-
-//Reference: https://codedestine.com/aws-s3-putobject-java/
-//Reference: https://stackoverflow.com/questions/42230086/writing-a-file-into-amazon-s3-aws-storage-using-scala
-
-
-object callings3:
-  val config = ConfigFactory.load("application")
-  val bucketName: String = config.getString("s3utilities.s3bucket")
-  val filePath: String = config.getString("s3utilities.file_path")
-  val fileName: String = config.getString("s3utilities.key")
-
-  val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_1).build
-  try s3.putObject(bucketName, filePath, new File(fileName))
-
-  catch {
-    case e: AmazonServiceException =>
-      System.err.println(e)
-      System.exit(1)
-  }
+//import com.amazonaws.AmazonServiceException
+//import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+//import com.amazonaws.regions.Regions
+//import com.typesafe.config.ConfigFactory
+//
+//import java.io.File
+//
+////Reference: https://codedestine.com/aws-s3-putobject-java/
+////Reference: https://stackoverflow.com/questions/42230086/writing-a-file-into-amazon-s3-aws-storage-using-scala
+//
+//object callings3:
+//  val config = ConfigFactory.load("application")
+//  val bucketName: String = config.getString("s3utilities.s3bucket")
+//  val filePath: String = config.getString("s3utilities.file_path")
+//  val fileName: String = config.getString("s3utilities.key")
+//
+//  val s3: AmazonS3 = AmazonS3ClientBuilder.standard.withRegion(Regions.US_EAST_1).build
+//  try s3.putObject(bucketName, filePath, new File(fileName))
+//
+//  catch {
+//    case e: AmazonServiceException =>
+//      System.err.println(e)
+//      System.exit(1)
+//  }
